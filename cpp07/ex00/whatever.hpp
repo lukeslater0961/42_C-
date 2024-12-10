b12fe0ce77da5dc79873f9f0987fa04955d42a72
Luke Slater <lslater@3G2.42angouleme.fr>
2024-12-10 11:04:06 +0100

    started cpp07 ex00 just need to test what ive done and continue with the other exersises

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,24 @@
+#ifndef	WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <utility>
+
+template <typename T>
+T	swap(T num1, T num2)
+{
+	std::swap(num1, num2);
+}
+
+template <typename T>
+T	min(T num1, T num2)
+{
+	return ((num1 > num2) ? num2: num1);
+}
+
+template <typename T>
+T	max(T num1, T num2)
+{
+	return ((num1 > num2) ? num1: num2);
+}
+
+#endif
