017aa25533e1fb36b12576e15bbcab37bf5ae430
Luke Slater <luke.a.slater@outlook.com>
2025-01-15 10:24:27 +0100

    rfeqwfqw

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,34 +1,13 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <iostream>
+#include <deque>
+#include <stack>
+
 template <typename T>
-class MutantStack{
-    public:
-        typedef typename    std::deque::iterator        const_iterator;
-        typedef typename    std::deque::const_iterator  const_iterator; 
+class MutantStack : public std::stack<T> {
+public:
+};
 
-        MutantStack(){}
-
-        MutantStack(cosnt MutantStack& other){stack(other.stack);}
-
-        MutantStack& operator=(const MutantStack& other)
-        {
-            if (this != &other)
-                this->stack = other.stack
-            return (*this);
-        }
-
-        ~MutantStack(){};
-
-        void    push(const T& value){stack.push(value);}
-        void    pop(){stack.pop();}
-        T&      top(){return stack.top();}
-        size_t  size(){return stack.size();}
-        bool    empty(){return stack.empty();}
-
-    private:
-        std::stack <T> stack;
-
-}
-
-#endif 
+#endif
